781f699822014be9b1c8eda62d13f8bd3e2bb062
Tim Harding <Tim@TimHarding.co>
2024-09-12 16:39:39 -0700

    Read from stdin

--- src/main.mjs
+++ src/main.mjs
@@ -4,10 +4,13 @@ import { TeX } from "mathjax-full/js/input/tex.js";
 import { SVG } from "mathjax-full/js/output/svg.js";
 import { liteAdaptor } from "mathjax-full/js/adaptors/liteAdaptor.js";
 import { RegisterHTMLHandler } from "mathjax-full/js/handlers/html.js";
+import { stdin } from "node:process";
+import { readFileSync } from "node:fs";
 
 AllPackages.push("colorv2");
 
 function main() {
+  const input = readFileSync(stdin.fd, "utf-8");
   const tex = new TeX({ packages: AllPackages });
   const svg = new SVG({});
   const adapter = liteAdaptor();
@@ -16,7 +19,7 @@ function main() {
     InputJax: tex,
     OutputJax: svg,
   });
-  const node = document.convert("\\frac{10}{20} = 0.5", {});
+  const node = document.convert(input, {});
   const out = adapter.innerHTML(node);
   console.log(out);
 }
